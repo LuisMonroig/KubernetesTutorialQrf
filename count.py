3939ee6f825b5baf16e3cf91dc4ade86335c5a06
LuisMonroig <luisemp96@gmail.com>
2023-01-27 06:32:49 -0500

    web app count.py

---
+++ count.py
@@ -0,0 +1,32 @@
+#app.py
+# code taken from https://lin-chen-langley.github.io/Kubernetes/Count.htm
+import os
+import time
+import redis
+from flask import Flask
+ 
+def get_ip():
+    return os.getenv('MYREDIS_HOST', 'localhost')
+ 
+app = Flask(__name__)
+cache = redis.Redis(host=get_ip(), port=6379)
+ 
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+ 
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
